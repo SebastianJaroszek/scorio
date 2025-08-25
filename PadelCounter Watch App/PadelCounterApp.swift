777c292478d07f0ce4a9b3f35545ba5efc85db7f
Sebastian Jaroszek <sebastian.jaroszek@cgm.com>
2025-08-25 16:19:00 +0200

    Initial Commit

---
+++ PadelCounter Watch App/PadelCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelCounterApp.swift
+//  PadelCounter Watch App
+//
+//  Created by Sebastian Jaroszek on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PadelCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
